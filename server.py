1d078bbfff3aafc4f5ec187dc4223e82d231d5a5
suzennn <52767681+suzennn@users.noreply.github.com>
2019-08-23 10:08:55 +0800

    added form for users to upload Store names into database

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import peeweedbevolve   #must be imported before models
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, render_template, request, redirect, flash
+from models import db, Store
 app = Flask(__name__)
 
 @app.before_request
@@ -19,6 +19,17 @@ def migrate():
 @app.route("/")
 def index():
    return render_template('index.html')
+
+@app.route("/store",methods=["GET"])
+def store():
+   return render_template('store.html')
+
+@app.route("/store",methods=["POST"])
+def add_store():
+   store_name = request.form["store_name"]
+   Store.create(name=store_name)
+   flash("Successfully saved!")
+#    return redirect("/")
 
 if __name__ == '__main__':
    app.run()
